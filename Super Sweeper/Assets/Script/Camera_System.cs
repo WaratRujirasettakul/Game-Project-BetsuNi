ab419b85c9a34b61923a2bfc9f5f17f9e74d7d37
Arcafanetiz <Arcafanetiz@users.noreply.github.com>
2019-10-02 15:32:06 +0700

    Adding Camera System

---
+++ Super Sweeper/Assets/Script/Camera_System.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_System : MonoBehaviour
+{
+    public Transform Camera;
+    public Transform Player;
+    void Update()
+    {
+        Camera.position = new Vector3(Player.position.x, 0, -10f);
+    }
+}
---
+++ Super Sweeper/Assets/Script/Gui_System.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gui_System : MonoBehaviour
+{
+    public Transform Player;
+    public Animator Animator;
+    private void FixedUpdate()
+    {
+        if (Player.position.y < -10)
+        {
+            if (!Animator.GetBool("Teleport"))
+            {
+                Animator.SetBool("Teleport", true);
+            }
+            else
+            {
+                if (Animator.GetCurrentAnimatorStateInfo(0).IsName("Fade_In") && Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+                {
+                    Player.position = Vector2.zero;
+                    Animator.SetBool("Teleport", false);
+                }
+            }
+        }
+    }
+}
