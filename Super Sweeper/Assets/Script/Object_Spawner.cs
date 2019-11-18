af55ca1e3670768b171a1fc615031cbaa75d2916
Arcafanetiz <54356299+Arcafanetiz@users.noreply.github.com>
2019-11-18 20:01:31 +0700

    Beta 0.1
    - Knock knock! Who's tere!
    - Zombie!
    - Zombie Who
    - Zombie Sweepis!

--- Super Sweeper/Assets/Script/Object_Spawner.cs
+++ Super Sweeper/Assets/Script/Object_Spawner.cs
@@ -6,10 +6,14 @@ public class Object_Spawner : MonoBehaviour
     [Header("Reference")]
     public GameObject Zombie;
     public GameObject Zombie_Waiter;
+    public GameObject Door_Pop;
+    public GameObject Knock_1;
+    public GameObject Knock_2;
+    public GameObject Knock_3;
     [Header("Component")]
     public SpriteRenderer Renderer;
     [Header("Property")]
-    [Range(1, 3)] public int Level = 1;
+    [Range(1, 8)] public int Level = 1;
     public float Spawn_Range;
     public float Spawn_Cooldown_Minimum;
     public float Spawn_Cooldown_Maximum;
@@ -17,16 +21,16 @@ public class Object_Spawner : MonoBehaviour
     private Transform Player;
     private bool Spawn_Enabled = false;
     private bool Spawn_Cooldown = false;
+    private bool Spawn_Opening = false;
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
     private void FixedUpdate()
     {
-        if (Mathf.Abs(Player.position.x - transform.position.x) < Spawn_Range && !Spawn_Enabled)
+        if (Mathf.Abs(Player.position.x - transform.position.x) < Spawn_Range && !Spawn_Enabled && !Spawn_Opening)
         {
-            Renderer.color = new Color(0, 0, 0);
-            Spawn_Enabled = true;
+            StartCoroutine(Open());
         }
         else if (Spawn_Enabled && !Spawn_Cooldown)
         {
@@ -55,5 +59,69 @@ public class Object_Spawner : MonoBehaviour
         }
         yield return new WaitForSeconds(Random.Range(Spawn_Cooldown_Minimum, Spawn_Cooldown_Maximum));
         Spawn_Cooldown = false;
+    }
+    private IEnumerator Open()
+    {
+        Spawn_Opening = true;
+        int Rand = Random.Range(1, 3);
+        if (Rand == 1)
+        {
+            Transform Knock = Knock_1.GetComponent<Transform>();
+            SpriteRenderer Knock_Render = Knock_1.GetComponent<SpriteRenderer>();
+
+            Knock.localPosition = new Vector3(Random.Range(-0.25f, 1f), Random.Range(-1f, 2f), 0);
+            Knock_Render.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+            Knock_Render.color = new Color(1, 1, 1, 0);
+            for (int i = 0; i <= Random.Range(1, 4); ++i)
+            {
+                yield return new WaitForSeconds(Random.Range(0.25f, 0.75f));
+                Knock.localPosition = new Vector3(Random.Range(-0.25f, 1f), Random.Range(-1f, 2f), 0);
+                Knock_Render.color = new Color(1, 1, 1, 1);
+                yield return new WaitForSeconds(0.1f);
+                Knock_Render.color = new Color(1, 1, 1, 0);
+            }
+        }
+        else if (Rand == 2)
+        {
+            Transform Knock = Knock_2.GetComponent<Transform>();
+            SpriteRenderer Knock_Render = Knock_2.GetComponent<SpriteRenderer>();
+
+            Knock.localPosition = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-1f, 1f), 0);
+            Knock_Render.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+            Knock_Render.color = new Color(1, 1, 1, 0);
+            for (int i = 0; i <= Random.Range(1, 4); ++i)
+            {
+                yield return new WaitForSeconds(Random.Range(0.25f, 0.75f));
+                Knock.localPosition = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-1f, 1f), 0);
+                Knock_Render.color = new Color(1, 1, 1, 1);
+                yield return new WaitForSeconds(0.1f);
+                Knock_Render.color = new Color(1, 1, 1, 0);
+            }
+        }
+        else if (Rand == 3)
+        {
+            Transform Knock = Knock_3.GetComponent<Transform>();
+            SpriteRenderer Knock_Render = Knock_3.GetComponent<SpriteRenderer>();
+
+            Knock.localPosition = new Vector3(Random.Range(-0.25f, 1f), Random.Range(-1f, 2f), 0);
+            Knock_Render.color = new Color(1, 1, 1, 1);
+            yield return new WaitForSeconds(0.1f);
+            Knock_Render.color = new Color(1, 1, 1, 0);
+            for (int i = 0; i <= Random.Range(1, 4); ++i)
+            {
+                yield return new WaitForSeconds(Random.Range(0.25f, 0.75f));
+                Knock.localPosition = new Vector3(Random.Range(-0.25f, 1f), Random.Range(-1f, 2f), 0);
+                Knock_Render.color = new Color(1, 1, 1, 1);
+                yield return new WaitForSeconds(0.1f);
+                Knock_Render.color = new Color(1, 1, 1, 0);
+            }
+        }
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+        Door_Pop.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        Door_Pop.GetComponent<Rigidbody2D>().AddForceAtPosition(new Vector2(Random.Range(-5f, 5f), Random.Range(0f, 5f)), new Vector2(Random.Range(-1f, 1f), Random.Range(-2f, 2f)));
+        Spawn_Enabled = true;
+        Destroy(Door_Pop, 4);
     }
 }
