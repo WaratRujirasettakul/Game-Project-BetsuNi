14dd5745b94e5a8d6623e0160860dfc4213c7ef6
Arcafanetiz <54356299+Arcafanetiz@users.noreply.github.com>
2019-11-20 10:59:21 +0700

    Beta 1.0 (Suppressed)
    - Some more bug fixes!
    - Another bit of everything!
    - It's suppressed tho

--- Super Sweeper/Assets/Script/Level_Save.cs
+++ Super Sweeper/Assets/Script/Level_Save.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class Level_Save
 {
-    public static bool Level1 = false;
-    public static bool Level2 = false;
+    public static bool Level1 = true;
+    public static bool Level2 = true;
     public static bool Level3 = false;
     public static bool Level4 = false;
     public static bool Level5 = false;
