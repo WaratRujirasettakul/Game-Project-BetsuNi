089b25171c5ae73784661c124cacb97d1dbb42f6
Arcafanetiz <54356299+Arcafanetiz@users.noreply.github.com>
2019-12-01 23:34:19 +0700

    Master 0.7
    - Stage 7 and 8.
    - Everything related to above.

--- Super Sweeper/Assets/Script/Level_Save.cs
+++ Super Sweeper/Assets/Script/Level_Save.cs
@@ -9,4 +9,6 @@ public static class Level_Save
     public static bool Level3 = false;
     public static bool Level4 = false;
     public static bool Level5 = false;
+    public static bool Level6 = false;
+    public static bool Level7 = false;
 }
